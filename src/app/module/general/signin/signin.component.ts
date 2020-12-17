90a323c2a07cd8945ec01fb79e08fbb5c4d59bcf
RSN <rsn3230@gmail.com>
2020-12-17 22:37:45 +0530

    commit

--- src/app/module/general/signin/signin.component.ts
+++ src/app/module/general/signin/signin.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { $ } from 'protractor';
 
 @Component({
   selector: 'app-signin',
@@ -10,6 +11,45 @@ export class SigninComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+
+    (function () {
+      console.log('Started****');
+      debugger;
+          // if ('OTPCredential' in window) {
+              debugger;
+              window.addEventListener('DOMContentLoaded', e => {
+                  debugger;
+              const input = document.querySelector('input[autocomplete="one-time-code"]');
+              if (!input) return;
+              const ac = new AbortController();
+              const form = input.closest('form');
+              debugger;
+              if (form) {
+                  debugger;
+                  form.addEventListener('submit', e => {
+                  ac.abort();
+                  });
+              }
+              navigator.credentials.get({
+                  // otp: { transport:['sms'] },
+                  signal: ac.signal
+              }).then(otp => {
+                  debugger;
+                  alert('OTP is***'+otp);
+                  // input.value = otp && otp.code ? otp.code : null;
+                  if (form) form.submit();
+              }).catch(err => {
+                  debugger;
+                  console.log(err);
+              });
+              });
+          // }else{
+          //   debugger;
+          //     alert('OTPCredential not found in window object.')
+          // }
+  
+      });
+
   }
 
 }
