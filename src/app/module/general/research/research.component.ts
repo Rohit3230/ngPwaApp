0aed48716a350bebbbcd1d82d23ff40eca8bdc39
RSN <rsn3230@gmail.com>
2020-12-18 13:53:06 +0530

    commit

--- src/app/module/general/research/research.component.ts
+++ src/app/module/general/research/research.component.ts
@@ -9,7 +9,7 @@ import {WebcamImage, WebcamInitError, WebcamUtil} from 'ngx-webcam';
 })
 export class ResearchComponent implements OnInit {
 
-  isCameraOpene:boolean = false;
+  // isCameraOpene:boolean = false;
   constructor() { }
 
   // ngOnInit(): void {
@@ -17,13 +17,13 @@ export class ResearchComponent implements OnInit {
 
 
   // toggle webcam on/off
-  public showWebcam = true;
+  public showWebcam = false;
   public allowCameraSwitch : boolean = true;
   public multipleWebcamsAvailable = false;
   public deviceId: string='';
   public videoOptions: MediaTrackConstraints = {
-    // width: {ideal: 1024},
-    // height: {ideal: 576}
+    width: {ideal: 1024},
+    height: {ideal: 576}
   };
   public errors: WebcamInitError[] = [];
 
